87fd60dc2e88c9703517a61e5c8edc035f36929c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-31 19:10:49 +0700

    feat(app.tsx): fetch product and log with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Product } from './@types/Product';
+import http from './utils/http';
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    const fetchProducts = async () => await http.get<Product[]>('/products').then(res => res.data)
+
+    fetchProducts().then(payload => setProducts(payload))
+  }, [])
+
+  useEffect(() => {
+    console.log(products)
+  }, [products])
+
   return (
     <div className="App">
       <h1>Hello this is Reactjs</h1>
