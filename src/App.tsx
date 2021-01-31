eb56112c5deef8315365c44b4332241e9577fa7b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-31 19:19:50 +0700

    feat(app.tsx): fetch products and show the results to browser

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,37 @@ function App() {
     fetchProducts().then(payload => setProducts(payload))
   }, [])
 
-  useEffect(() => {
-    console.log(products)
-  }, [products])
-
   return (
     <div className="App">
       <h1>Hello this is Reactjs</h1>
+      <p>
+        Api resource : <a href="http://localhost:9000/api/products">http://localhost:9000/api/products</a>
+      </p>
+      <hr />
+      {products.length ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Discount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <tr>
+                <td>{ product.id }</td>
+                <td>{ product.name }</td>
+                <td>{ product.description }</td>
+                <td>{ product.price }</td>
+                <td>{ product.discount }%</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
     </div>
   );
 }
